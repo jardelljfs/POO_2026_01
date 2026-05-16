ddf1c7a6aad9081df24ef92efce43b55afd69ebc
jardell <jardell.jfs@gmail.com>
2026-05-16 11:52:08 -0300

    Prova II

---
+++ Prova_I.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+#pragma warning disable
+
+class Aluno
+{
+    private string nome;
+    private double nota;
+
+    public Aluno(string nome, double nota)
+    {
+        this.nome = nome;
+        this.nota = nota;
+    }
+
+    public string getNome()
+    {
+        return this.nome;
+    }
+
+    public double getNota()
+    {
+        return this.nota;
+    }
+
+    public void setNota(double nota)
+    {
+        if(nota >= 0){
+            this.nota = nota;
+        }
+        else{
+            Console.WriteLine("A nota precisa ser maior que 0");        
+        }
+    }
+
+    public void exibirAluno()
+    {
+        Console.WriteLine("\nNome do aluno: " + this.nome);
+        Console.WriteLine("Nota do aluno: " + this.nota);
+    }
+	
+}
+				
+class Program{
+	public static void Main(){
+        Aluno a1 = new Aluno("José", 7.0);
+        Aluno a2 = new Aluno("Maria", 8.0);
+
+        a1.setNota(9.0);
+
+        a1.exibirAluno();
+        a2.exibirAluno();
+	}
+}
---
+++ Prova_II.cs
@@ -0,0 +1,31 @@
+﻿class Funcionario {
+    protected string nome;
+    protected double salario;
+
+    public Funcionario(string nome, double salario) {
+        this.nome    = nome;
+        this.salario = salario;
+    }
+
+    public virtual void ExibirInfo() { Console.Write( $"Funcionário: {nome}" + $" | Salário: R$ {salario}"); }
+}
+
+class Gerente : Funcionario {
+    private string departamento;
+
+    public Gerente(string nome, double salario, string departamento) : base(nome, salario) {
+        this.departamento = departamento;
+    }
+
+    public override void ExibirInfo() {
+        base.ExibirInfo();
+        Console.WriteLine($" | Depto: {departamento}");
+    }
+}
+
+class Programa {
+    static void Main() {
+        Funcionario g = new Gerente("Ana", 8000, "TI");
+        g.ExibirInfo();
+    }
+}
